b82a150d583e0e0691db67b9f5a5ce2efa83dda8
Piyush Assudani <63794264+assudani66@users.noreply.github.com>
2023-07-09 21:57:23 +0530

    1st push

--- src/app/[locationId]/[placeId]/page.tsx
+++ src/app/[locationId]/[placeId]/page.tsx
@@ -2,10 +2,10 @@ import { data } from '@/data'
 import React from 'react'
 
 const Page = ({params} : {params:{locationId:string,placeId:string}}) => {
-  const placeDetails = data?.continents?.countries?.find(({id})=>id.toString()===params.placeId)
+  // const placeDetails = data?.continents?.countries?.find(({id})=>id.toString()===params.placeId)
   return (
     <div>
-      <img className="w-80 h-80" src={placeDetails?.image}/>
+      {/* <img className="w-80 h-80" src={placeDetails?.image}/> */}
     </div>
   )
 }
